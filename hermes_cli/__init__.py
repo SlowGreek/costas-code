e8b325d78c53fb3c69c0d3d7e8a9d25349069484
Costas Panay <costaspanay@microsoft.com>
2026-07-29 13:43:45 -0700

    chore: bump version to v0.21.0 (2026.7.29)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -14,8 +14,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.20.0"
-__release_date__ = "2026.7.28"
+__version__ = "0.21.0"
+__release_date__ = "2026.7.29"
 
 
 def _ensure_utf8():
